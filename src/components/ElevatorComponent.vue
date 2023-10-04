61a00403bad5757dba0b7820e118273d5cd23f93
Ivan Nor <ivan.norik@gmail.com>
2023-10-04 15:37:47 +0300

    add computed style transition

--- src/components/ElevatorComponent.vue
+++ src/components/ElevatorComponent.vue
@@ -12,38 +12,43 @@ export default {
   },
   data () {
     return {
-      elevatorStyle: {
-        'margin-bottom': 10
-      },
-      currentFloor: 1
+      // elevatorStyle: {
+      //   'margin-bottom': '10px'
+      // }
     }
   },
   watch: {
-    currentFloor (newFloor, oldFloor) {
-      console.log('watch curr floor')
-      this.moveElevator(newFloor, oldFloor)
+    // elevatorStyle: function (prev, next) {
+    //   const prevValue = prev['margin-bottom'].match(/^\d+/)[0]
+    //   const nextValue = next['margin-bottom'].match(/^\d+/)[0]
+    //   console.log('WATCH', prev, next, Math.abs(prevValue - nextValue))
+    // }
+  },
+  computed: {
+    elevatorStyle () {
+      const { currentFloor, prevFloor } = this.elevator
+      return ({ 'margin-bottom': `${10 + (this.elevator.currentFloor - 1) * 50}px`, transition: `margin-bottom ${Math.abs(currentFloor - prevFloor)}s ease` })
     }
   },
   updated () {
-    console.log('elev upd', this.elevator, this.elevator.currentFloor, this.elevatorStyle)
-    this.elevatorStyle['margin-bottom'] = `${10 + (this.elevator.currentFloor - 1) * 50}px`
+    console.log('update ELEVATOR', this.elevator, this.elevator.currentFloor, this.elevatorStyle)
   },
   methods: {
-    moveElevator (newFloor, oldFloor) { // ДОЛЖНА БЫТЬ АНИМАЦИЯ ЗДЕСЬ ?
-      console.log('move elevator')
-      // const floorHeight = 100
+    // moveElevator (newFloor, oldFloor) { // ДОЛЖНА БЫТЬ АНИМАЦИЯ ЗДЕСЬ ?
+    //   console.log('move elevator', newFloor, oldFloor)
+    // const floorHeight = 100
 
-      // const positionChange = (oldFloor - newFloor) * floorHeight
+    // const positionChange = (oldFloor - newFloor) * floorHeight
 
-      // const elevatorElement = this.$ref.querySelector('.elevator')
-      // elevatorElement.style.transition = 'bottom 1s ease'
-      // elevatorElement.style.bottom = `${positionChange}px`
+    // const elevatorElement = this.$ref.querySelector('.elevator')
+    // elevatorElement.style.transition = 'bottom 1s ease'
+    // elevatorElement.style.bottom = `${positionChange}px`
 
-      // setTimeout(() => {
-      //   elevatorElement.style.transition = 'none'
-      //   elevatorElement.style.bottom = '0px'
-      // }, 500)
-    }
+    // setTimeout(() => {
+    //   elevatorElement.style.transition = 'none'
+    //   elevatorElement.style.bottom = '0px'
+    // }, 500)
+    // }
   }
 }
 </script>
